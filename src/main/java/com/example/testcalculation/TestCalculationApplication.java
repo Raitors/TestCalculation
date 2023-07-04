2a9a05a34c2a1e256344635952fe006c97a54d85
Shashkin <artyom.sh.s@yandex.ru>
2023-07-04 13:05:27 +0300

    init commit

---
+++ src/main/java/com/example/testcalculation/TestCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCalculationApplication.class, args);
+    }
+
+}
